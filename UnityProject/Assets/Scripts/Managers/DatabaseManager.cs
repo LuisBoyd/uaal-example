0bf890cba0ff295fc449ab61f4855daf91e15fc7
LuisBoyd <97450900+WizardBoyd@users.noreply.github.com>
2022-11-01 16:10:24 +0000

    DLL's be careful

---
+++ UnityProject/Assets/Scripts/Managers/DatabaseManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+namespace RCR.Managers
+{
+    public static class DatabaseManager
+    {
+        private const string m_databaseConnectionInfo =
+            "server=82.165.169.82:8443;uid=admin_wnavadmin;pwd=Zk2~2wx4;database=WN_Game";
+
+        public static void TestConnection()
+        {
+            using (var sqlConnection = new MySqlConnection())
+            {
+                
+            }
+        }
+
+    }
+}
