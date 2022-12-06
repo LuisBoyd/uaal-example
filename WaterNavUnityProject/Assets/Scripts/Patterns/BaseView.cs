4194c50e3fcb368f4dfe3018442ea0e55d398302
LuisBoyd <97450900+WizardBoyd@users.noreply.github.com>
2022-12-06 16:44:45 +0000

    Fixed the Editor problem
    The Project had to be de-corrupted
    
    Look at the map sections on the database when they load into unity some of it is missing

---
+++ WaterNavUnityProject/Assets/Scripts/Patterns/BaseView.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+namespace RCR.Patterns
+{
+    public class BaseView<M,C> : MonoBehaviour where M: BaseModel, new()
+    where C : BaseController<M>, new()
+    {
+        public M Model;
+        protected C Controller;
+
+        /// <summary>
+        /// Set To True if there are any problems in which case it can be handled
+        /// </summary>
+        protected bool IsValid;
+
+        protected virtual void Awake()
+        {
+            IsValid = false;
+            Controller = new C();
+            Model = new M();
+            Controller.Setup(Model);
+        }
+    }
+}
